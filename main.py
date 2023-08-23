2c3876e1af4e8969b1c5fe137259f87b500acd47
hung.duong <hung.duong@be.com.vn>
2023-08-23 15:45:32 +0700

    refactor all

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+A custom from source with only support Tictactoe 3x3 to 
+input all config from user.
+This software is available under GPL license.
+Author: Clederson Cruz
+Developer with custom: Hung Duong
+Year: 2017
+License: GNU GENERAL PUBLIC LICENSE (GPL)
+"""
+
+from game.tictactoe import Tictactoe
+
+if __name__ == "__main__":
+    game = Tictactoe()
+    game.run()
